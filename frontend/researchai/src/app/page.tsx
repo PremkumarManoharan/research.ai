6133d77e023fabae856c1b4e9d826ed45909ad85
Varun Jayakumar <jayakumar.va@northeastern.edu>
2024-07-20 01:14:47 -0400

    added inital webpage

---
+++ frontend/researchai/src/app/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Image from "next/image";
+import { SparklesCore } from "../../components/ui/sparkles";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="bg-black">
+      <div className="h-[60em] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+          Reasearch.ai
+        </h1>
+        <p className="text-slate-300 mb-1">
+          Research.Ai: Transforming Reading into Understanding with AI
+          Precision.
+        </p>
+        <div className="w-[40rem] h-40 relative">
+          {/* Gradients */}
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+          {/* Core component */}
+          <SparklesCore
+            background="transparent"
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
+          {/* Radial Gradient to prevent sharp edges */}
+          <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+        </div>
+        <Button variant="secondary" size="lg">
+          Get Started for Free! 👋
+        </Button>
+      </div>
+    </div>
+  );
+}
