7e479b4559d103884ba3619f81c92452bd40bca6
Varun Jayakumar <jayakumar.va@northeastern.edu>
2024-07-20 01:18:50 -0400

    fixed typo

--- frontend/researchai/src/app/page.tsx
+++ frontend/researchai/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="bg-black">
       <div className="h-[60em] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
         <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
-          Reasearch.ai
+          Research.ai
         </h1>
         <p className="text-slate-300 mb-1">
           Research.Ai: Transforming Reading into Understanding with AI
