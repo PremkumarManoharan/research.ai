c4ca38695a1d01e7d9697423f30e77f7b8813db8
Varun Jayakumar <jayakumar.va@northeastern.edu>
2024-07-22 01:19:56 -0400

    added email box and layout

---
+++ frontend/researchai/src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Page() {
+  return (
+    <>
+      <div style={{ flex: "0 0 15%", backgroundColor: "#f0f0f0" }}>
+        file drawer
+      </div>
+      <div style={{ flex: "0 0 55%", backgroundColor: "#ffffff" }}>
+        Pdf Viewer
+      </div>
+      <div style={{ flex: "0 0 30%", backgroundColor: "#f0f0f0" }}>
+        Chat component
+      </div>
+    </>
+  );
+}
+
+export default Page;
--- frontend/researchai/src/app/page.tsx
+++ frontend/researchai/src/app/page.tsx
@@ -1,17 +1,40 @@
 "use client";
-import Image from "next/image";
-import { SparklesCore } from "../../components/ui/sparkles";
+import { SparklesCore } from "@/components/ui/sparkles";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import useStorage from "./../hooks/useStorage";
 
 export default function Home() {
+  const { setItem } = useStorage();
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  let email = "";
+  const router = useRouter();
+
+  const handleOnchange = (e: React.FormEvent<HTMLInputElement>) => {
+    let regexp = new RegExp(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+    regexp.test(e.currentTarget.value)
+      ? setIsButtonDisabled(false)
+      : setIsButtonDisabled(true);
+    email = e.currentTarget.value;
+  };
+
+  const handleOnClick = () => {
+    router.push("/dashboard");
+    setItem("email", email, "session");
+  };
+
   return (
     <div className="bg-black">
-      <div className="h-[60em] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <div className="h-screen w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
         <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
           Research.ai
         </h1>
         <p className="text-slate-300 mb-1">
-          Research.Ai: Transforming Reading into Understanding with AI
+          Research.ai: Transforming Reading into Understanding with AI
           Precision.
         </p>
         <div className="w-[40rem] h-40 relative">
@@ -32,9 +55,17 @@ export default function Home() {
           {/* Radial Gradient to prevent sharp edges */}
           <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
         </div>
-        <Button variant="secondary" size="lg">
+        <Input onChange={handleOnchange} width="20%" />
+
+        <Button
+          disabled={isButtonDisabled}
+          variant="secondary"
+          size="lg"
+          onClick={handleOnClick}
+        >
           Get Started for Free! 👋
         </Button>
+        <div />
       </div>
     </div>
   );
